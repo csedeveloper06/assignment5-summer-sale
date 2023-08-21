631963f3e9b58d10d614d7f4e9cebb2a4ab7e959
cseDeveloper06 <faridayasmeencse@gmail.com>
2023-08-21 22:38:23 +0600

    added updates

---
+++ js/summer-sales.js
@@ -0,0 +1,16 @@
+let total = 0;
+function handleClick(target){
+    const selectedItemsContainer = document.getElementById('itemsId');
+    const elementName = target.parentElement.parentElement.nextSibling.nextSibling.childNodes[3].innerText; 
+    console.log(elementName);
+    const li = document.createElement("li");
+    li.innerText = elementName;
+    selectedItemsContainer.appendChild(li);
+    const price = target.parentElement.parentElement.nextSibling.nextSibling.childNodes[5].innerText.split(' ')[0];
+    console.log(price);
+    total = parseFloat(total) + parseFloat(price);
+    total = total.toFixed(2);
+    console.log(total);
+    document.getElementById('totalPriceId').innerText = total;
+
+}
